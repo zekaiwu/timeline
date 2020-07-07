9ae443767d78dae9aaaf8bdca2e3503d8ca10efa
zekaiwu <u3546858@connect.hku.hk>
2020-07-07 17:01:49 +0800

    add item update item

--- timeline.js
+++ timeline.js
@@ -4,14 +4,14 @@ export function showTimeLine(recipe) {
   let actions = recipe.actions;
   let startTime = new Date(2020, 0, 1, 0, 0, 0);
   let endTime = new Date(2020, 0, 1, 0, 10, 0);
-  let length = 60000;
+  let length = 3000;
   let items = new vis.DataSet();
-  for (let i=0;i<actions.length;i++){
+  for (let i = 0; i < actions.length; i++) {
     items.add({
       id: i,
       content: actions[i].command,
-      start: new Date(2020, 0, 1, 0, 1, 0),
-      end: new Date(2020, 0, 1, 0, 2, 0),
+      start: new Date().setTime(startTime.getTime() + actions[i].time * 1000),
+      end: new Date().setTime(startTime.getTime() + actions[i].time * 1000 + length),
       length: length,
     })
   }
@@ -20,6 +20,7 @@ export function showTimeLine(recipe) {
 
   // Configuration for the Timeline
   var options = {
+    timeAxis: {scale: 'second', step: 5},
     min: startTime,
     max: endTime,
     start: startTime,
@@ -31,9 +32,9 @@ export function showTimeLine(recipe) {
     },
     format: {
       minorLabels: {
-        millisecond: 'mm:ss',
-        second: 'mm:ss',
-        minute: 'mm:ss',
+        millisecond: 'ss',
+        second: 'ss',
+        minute: 'ss',
         hour: '',
         weekday: '',
         day: '',
@@ -56,7 +57,10 @@ export function showTimeLine(recipe) {
     onAdd: function(item, callback) {
       prettyPrompt('Add item', 'Enter text content for new item:', item.content, function(value) {
         if (value) {
-          item.content = value;
+          item.content = value[0]+' '+value[1];
+          item.start=new Date().setTime(startTime.getTime() + value[2] * 1000);
+          item.length=length;
+          item.end= new Date().setTime(startTime.getTime() + value[2] * 1000 + length),
           callback(item); // send back adjusted new item
         } else {
           callback(null); // cancel item creation
@@ -65,10 +69,11 @@ export function showTimeLine(recipe) {
     },
 
     onUpdate: function(item, callback) {
-
       prettyPrompt('Update item', 'Edit items text:', item.content, function(value) {
         if (value) {
-          item.content = value;
+          item.content = value[0]+' '+value[1];
+          item.start=new Date().setTime(startTime.getTime() + value[2] * 1000);
+          item.end= new Date().setTime(startTime.getTime() + value[2] * 1000 + length),
           callback(item); // send back adjusted item
         } else {
           callback(null); // cancel updating the item
@@ -121,15 +126,30 @@ export function showTimeLine(recipe) {
 
   }
   */
-  function prettyPrompt(title, text, inputValue, callback) {
-    swal({
-      title: title,
-      text: text,
-      type: 'input',
-      showCancelButton: true,
-      inputValue: inputValue
-    }, callback);
-  }
+  async function prettyPrompt(title, text, inputValue, callback) {
+    const { value: formValues } = await Swal.fire({
+      title: 'Multiple inputs',
+      html:
+        'function'+
+        '<select id="swal-input1" class="swal2-input" list="swal-input1" name="swal-input1">'+
+        '<option value="WORKTEMP">WORKTEMP</option>'+
+        '<option value="POURBOX">POURBOX</option>'+
+        '</select>'+
+        'parameter<input id="swal-input2" class="swal2-input">'+
+        'time<input id="swal-input3" class="swal2-input">',
+      focusConfirm: false,
+      preConfirm: () => {
+        return [
+          document.getElementById('swal-input1').value,
+          document.getElementById('swal-input2').value,
+          document.getElementById('swal-input3').value,
+        ]
+      }
+    })
+    if (formValues) {
+      callback(formValues);
+    }
+}
 
   function prettyConfirm(title, text, callback) {
     swal({
